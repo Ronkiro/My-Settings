5ba72d3b0dcea2aa15e338692c309cf08f4ccbe3
Alexander Santos <alexander.santos@rio.msf.org>
2020-01-02 13:22:39 -0300

    Add pwsh profile & vscode settings.json

---
+++ Microsoft.PowerShell-Profile.ps1
@@ -0,0 +1,6 @@
+# THEME COLOR = Argonaut
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+# Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-Theme Paradox
